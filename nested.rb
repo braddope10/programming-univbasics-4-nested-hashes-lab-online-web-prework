825a86dd700e81b48e979cf5d3f1b13421d91495
Bradley Lopez <bradleylopezherrera@gmail.com>
2019-09-27 21:30:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,7 +77,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+	programmer_hash1 = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -93,11 +93,12 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {
+  programmer_hash1[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
-    
+  
+  return programmer_hash1
 
 end
 

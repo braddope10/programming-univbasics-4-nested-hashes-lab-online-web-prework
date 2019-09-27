2225a5d917009381627c5097cd11f1a7e08e50d8
Bradley Lopez <bradleylopezherrera@gmail.com>
2019-09-27 21:37:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,11 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-  programmer_hash2[:known_for] = [:alans_new_info]
+  programmer_hash2[:dennis_ritchie][:known_for] = [:alans_new_info]
+  
+  programmer_hash2[:alans_new_info] = "GUI"
+  
+  
 end
 
 def adding_to_dennis
